1050ecc3420f6a1994f8847ea918809031cd8586
Stephen Klancher <smknight@gmail.com>
2021-10-02 12:06:25 -0700

    Lines UI sortable by timestamp without sorting backing list

--- EventLogAnalyzer/EventLogAnalyzer/LinesListView.cs
+++ EventLogAnalyzer/EventLogAnalyzer/LinesListView.cs
@@ -23,6 +23,7 @@ namespace EventLogAnalyzer
             list.VirtualListSize = 0;
             list.RetrieveVirtualItem += List_RetrieveVirtualItem;
             list.SelectedIndexChanged += List_SelectedIndexChanged;
+            list.ColumnClick += List_ColumnClick;
 
             DetailText = detail;
             DebugProperties = propertyGrid;
@@ -31,10 +32,16 @@ namespace EventLogAnalyzer
         }
 
         public EventCollection CurrentLines { get; private set; } = new();
+
         public PropertyGrid DebugProperties { get; }
+
         public TextBox DetailText { get; }
+
         public List<string> InternalLog { get; set; } = new();
+
         public bool IsDisplayingInternalLog { get; private set; } = false;
+
+        public bool SortNewestFirst { get; set; } = true;
 
         private ListView list { get; }
 
@@ -47,27 +54,27 @@ namespace EventLogAnalyzer
             IsDisplayingInternalLog = true;
             list.VirtualListSize = InternalLog.Count;
 
-            SelectLineLine();
+            SelectMostRecentLine();
 
             list.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
             list.EndUpdate();
         }
 
-        public void SelectFirstLine()
+        public void SelectEarliestLine()
         {
             list.SelectedIndices.Clear();
             if (list.VirtualListSize > 0)
             {
-                list.SelectedIndices.Add(0);
+                list.SelectedIndices.Add(ReversableIndex(0, list.VirtualListSize, SortNewestFirst));
             }
         }
 
-        public void SelectLineLine()
+        public void SelectMostRecentLine()
         {
             list.SelectedIndices.Clear();
             if (list.VirtualListSize > 0)
             {
-                list.SelectedIndices.Add(list.VirtualListSize - 1);
+                list.SelectedIndices.Add(ReversableIndex(list.VirtualListSize - 1, list.VirtualListSize, SortNewestFirst));
             }
         }
 
@@ -78,23 +85,35 @@ namespace EventLogAnalyzer
             CurrentLines = newlines;
             list.VirtualListSize = CurrentLines.Count;
 
-            SelectFirstLine();
+            SelectMostRecentLine();
 
             list.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
             list.EndUpdate();
         }
 
+        private void List_ColumnClick(object? sender, ColumnClickEventArgs e)
+        {
+            if (e.Column == 0)
+            {
+                list.BeginUpdate();
+                SortNewestFirst = !SortNewestFirst;
+                list.Invalidate();
+                list.EndUpdate();
+            }
+        }
+
         private void List_RetrieveVirtualItem(object sender, RetrieveVirtualItemEventArgs e)
         {
             string[] LineInfo;
+            int index = ReversableIndex(e.ItemIndex, true);
 
             if (IsDisplayingInternalLog)
             {
-                LineInfo = new string[] { string.Empty, "Debug", InternalLog[e.ItemIndex] };
+                LineInfo = new string[] { string.Empty, "Debug", InternalLog[index] };
             }
             else
             {
-                var Line = CurrentLines[e.ItemIndex];
+                var Line = CurrentLines[index];
                 LineInfo = new string[] { TimestampOptions.ConvertToString(Line.Timestamp), Line.Level, Line.Message };
             }
 
@@ -108,18 +127,36 @@ namespace EventLogAnalyzer
                 return;
             }
 
+            int index = ReversableIndex(list.SelectedIndices[0], true);
+
             if (IsDisplayingInternalLog)
             {
-                var line = InternalLog[list.SelectedIndices[0]];
+                var line = InternalLog[index];
                 DetailText.Text = line ?? string.Empty;
                 DebugProperties.SelectedObject = null;
             }
             else
             {
-                var Line = CurrentLines[list.SelectedIndices[0]];
+                var Line = CurrentLines[index];
                 DebugProperties.SelectedObject = Line;
                 DetailText.Text = Line.Message;
             }
+        }
+
+        private int ReversableIndex(int index, bool indexFromUI = false) => ReversableIndex(index, list.VirtualListSize, SortNewestFirst, indexFromUI);
+
+        /// <summary>
+        /// Return the correct index according to sorting preference (avoiding sorting the backing list)
+        /// </summary>
+        /// <param name="index">input index</param>
+        /// <param name="count">count of the list (which should be identical to UI via VirtualListSize)</param>
+        /// <param name="reverseSortPreference">sort preference that can be changed from UI</param>
+        /// <param name="indexFromUI">Whether the index is from the UI (SelectedIndices,etc).  If false then expecting an index of the backing list.</param>
+        /// <returns></returns>
+        private int ReversableIndex(int index, int count, bool reverseSortPreference, bool indexFromUI = false)
+        {
+            bool reverse = reverseSortPreference ^ indexFromUI;
+            return Math.Abs((Convert.ToInt32(!reverse) * (count - 1)) - index);
         }
     }
 }
