cfcf59317f9424fb031b997cb24839997387aeeb
Stephen Klancher <smknight@gmail.com>
2023-05-18 17:20:29 -0700

    disable trait with poor performing regex

--- EventLogAnalyzer/EventLogAnalysis/TotalAgilityTraits.cs
+++ EventLogAnalyzer/EventLogAnalysis/TotalAgilityTraits.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -10,6 +11,13 @@ namespace EventLogAnalysis
     public class TotalAgilityTraits
     {
         public static void TotalAgilityTenantName(TraitTypeCollection traits, ELRecord r)
+        {
+            //splitting into separate functions to look into performance of each separately
+            // TenantNameForm1(traits, r); poor performing regex, disabled for now
+            TenantNameForm2(traits, r);
+        }
+
+        private static void TenantNameForm1(TraitTypeCollection traits, ELRecord r)
         {
             // language=regex
             string msgregex = @"\w+_(?<dep>live|dev)";
@@ -29,7 +37,10 @@ namespace EventLogAnalysis
                     r.AddTrait("TotalAgility Tenant Code", tenantcode, traits);
                 }
             }
+        }
 
+        private static void TenantNameForm2(TraitTypeCollection traits, ELRecord r)
+        {
             // language=regex
             string formtwo = @"Tenant Code\s-\s(?<name>\w+)";
 
